cb7dd14d20018204e3e232280c37573bdeb21359
mjeed <mj.naser2005@gmail.com>
2025-10-15 23:36:03 +0300

    Solving the palindrome problem

---
+++ ProblemSolving/RandomQuestions/BeginnerQuestions/src/beginnerquestions/BeginnerQuestions.java
@@ -0,0 +1,16 @@
+//@author mjnas
+package beginnerquestions;
+
+
+public class BeginnerQuestions {
+
+     
+    public static void main(String[] args) {
+      /* Here is a collection of classes. 
+        The class contains a question to solve a problem.
+     */
+
+   
+    }
+
+}
