00864a4a627839e390b67e842b8e89bedbc51643
mjeed <mj.naser2005@gmail.com>
2025-10-18 01:34:55 +0300

    add CodeForces file and solution Watermelon

---
+++ ProblemSolving/Codeforces/Level800/src/level800/Level800.java
@@ -0,0 +1,14 @@
+//@author mjnas
+package level800;
+
+
+public class Level800 {
+
+     
+    public static void main(String[] args) {
+      /*All questions solved at level 800 */
+
+   
+    }
+
+}
---
+++ ProblemSolving/Codeforces/Level800/src/level800/Watermelon.java
@@ -0,0 +1,18 @@
+package level800;
+
+import java.util.Scanner;
+
+public class Watermelon {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int kilos = input.nextInt();
+        if (kilos %2==0) {
+            System.out.println("YES");
+        }
+        else
+            System.out.println("No");
+
+    }
+
+}
