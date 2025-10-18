c2c1b9948fcd23e4af90c36fa11a3b2200effa27
mjeed <mj.naser2005@gmail.com>
2025-10-18 21:19:42 +0300

    add Team and Bit

--- ProblemSolving/Codeforces/Level800/src/level800/Watermelon.java
+++ ProblemSolving/Codeforces/Level800/src/level800/Watermelon.java
@@ -7,9 +7,9 @@ public class Watermelon {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int kilos = input.nextInt();
-        if (kilos % 2==0 && kilos>2) {
+        if (kilos % 2==0 && kilos>2) 
             System.out.println("YES");
-        }
+        
         else
             System.out.println("No");
 
--- ProblemSolving/Codeforces/Level800/src/level800/Way_Too_Long_Words.java
+++ ProblemSolving/Codeforces/Level800/src/level800/Way_Too_Long_Words.java
@@ -1,23 +1,23 @@
 package level800;
 
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class Way_Too_Long_Words {
+    public class Way_Too_Long_Words {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
 
-        int number = input.nextInt();
-        input.nextLine();
-        for (int i = 0; i < number; i++) {
-            String word = input.nextLine();
-            if (word.length() > 10) {
-                System.out.println(word.charAt(0) + "" + (word.length() - 2) + "" + word.charAt(word.length() - 1));
+            int number = input.nextInt();
+            input.nextLine();
+            for (int i = 0; i < number; i++) {
+                String word = input.nextLine();
+                if (word.length() > 10) {
+                    System.out.println(word.charAt(0) + "" + (word.length() - 2) + "" + word.charAt(word.length() - 1));
 
-            } else {
-                System.out.println(word);
+                } else {
+                    System.out.println(word);
+                }
+
             }
-
         }
     }
-}
