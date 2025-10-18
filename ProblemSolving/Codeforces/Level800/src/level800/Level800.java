1dbe785ba3d3e66a92ff2dde2c88f4c3ce5007f1
mjeed <mj.naser2005@gmail.com>
2025-10-18 18:44:54 +0300

    add Way_Too_Long_Words

--- ProblemSolving/Codeforces/Level800/src/level800/Level800.java
+++ ProblemSolving/Codeforces/Level800/src/level800/Level800.java
@@ -1,12 +1,27 @@
 //@author mjnas
 package level800;
 
+import java.util.Scanner;
+
 
 public class Level800 {
 
      
     public static void main(String[] args) {
       /*All questions solved at level 800 */
+      
+        Scanner input = new Scanner(System.in);
+         int x = input.nextInt(); 
+         input.nextLine();
+         for (int i = 0; i < x; i++) {
+            String word =input.nextLine();
+             if (word.length()>10) {
+                 System.out.println(word.charAt(0)+""+(word.length()-2)+""+ word.charAt(word.length()-1));
+                 
+             }
+             else
+                 System.out.println(word);
+        }
 
    
     }
--- ProblemSolving/Codeforces/Level800/src/level800/Way_Too_Long_Words.java
+++ ProblemSolving/Codeforces/Level800/src/level800/Way_Too_Long_Words.java
@@ -6,17 +6,16 @@ public class Way_Too_Long_Words {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int n = input.nextInt(); // عدد الكلمات
-        input.nextLine(); // للتخلص من السطر الجديد بعد الرقم
-
-        for (int i = 0; i < n; i++) {
-            String word = input.nextLine();
-            if (word.length() > 10) {
-                int middle = word.length() - 2;
-                System.out.println(word.charAt(0) + "" + middle + word.charAt(word.length() - 1));
-            } else {
-                System.out.println(word);
-            }
+         int number = input.nextInt(); 
+         input.nextLine();
+         for (int i = 0; i < number; i++) {
+            String word =input.nextLine();
+             if (word.length()>10) {
+                 System.out.println(word.charAt(0)+""+(word.length()-2)+""+ word.charAt(word.length()-1));
+                 
+             }
+             else
+                 System.out.println(word);
         }
     }
 }
