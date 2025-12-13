8a845c8caa37c9feb0aa054bf14d5b7f8927022d
mjeed <mj.naser2005@gmail.com>
2025-12-14 02:02:14 +0300

    End The front-End in The Progict GYM

--- ProblemSolving/Codeforces/Level800/src/level800/BoyOrGirl.java
+++ ProblemSolving/Codeforces/Level800/src/level800/BoyOrGirl.java
@@ -8,8 +8,29 @@ public class BoyOrGirl {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String x = input.next().toLowerCase();
-         
-         
+        
+        int count =0 ;
+        for (int i = 0; i < x.length(); i++) {
+            char c = x.charAt(i);
+            boolean n = true;
+            for (int j = 0; j < i; j++) {
+                if (c == x.charAt(j) ) {
+                    n=false; 
+                    break;
+                }
+                
+            }
+            if (n) {
+                count++; 
+            }
+        }
+        if ( count%2==0) {
+            System.out.println("CHAT WITH HER!");
+        }
+        else
+          System.out.println("IGNORE HIM!");
+   
+        
         
         
     }
---
+++ Swing/Gym management System/Gym/src/gym/Gym.java
@@ -0,0 +1,15 @@
+//@author mjnas
+package gym;
+
+
+public class Gym {
+
+     
+    public static void main(String[] args) {
+      
+        Member b = new Member(); 
+        b.add("mjeed", b);
+   
+    }
+
+}
