d0a8722d3e28effbd8bd01a17446ef625f532d96
mjeed <mj.naser2005@gmail.com>
2026-03-18 07:09:07 +0300

    BoyOrGirl

--- ProblemSolving/Codeforces/Level800/src/level800/BoyOrGirl.java
+++ ProblemSolving/Codeforces/Level800/src/level800/BoyOrGirl.java
@@ -1,38 +1,19 @@
- 
 package level800;
-
 import java.util.Scanner;
+import java.util.HashSet;  
 
- 
 public class BoyOrGirl {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String x = input.next().toLowerCase();
-        
-        int count =0 ;
+        String x = input.next();
+        HashSet<Character> uniqueChars = new HashSet<>();
         for (int i = 0; i < x.length(); i++) {
-            char c = x.charAt(i);
-            boolean n = true;
-            for (int j = 0; j < i; j++) {
-                if (c == x.charAt(j) ) {
-                    n=false; 
-                    break;
-                }
-                
-            }
-            if (n) {
-                count++; 
-            }
+            uniqueChars.add(x.charAt(i)); 
         }
-        if ( count%2==0) {
+        if (uniqueChars.size() % 2 == 0) {
             System.out.println("CHAT WITH HER!");
+        } else {
+            System.out.println("IGNORE HIM!");
         }
-        else
-          System.out.println("IGNORE HIM!");
-   
-        
-        
-        
     }
-
 }
